b3ed486dfd5081ded7219a59ba90f555ab76eff9
Varenic Victor <kabalx47@gmail.com>
2017-10-08 13:06:48 +0300

    basic

--- main.py
+++ main.py
@@ -1,48 +1,20 @@
 import time
 import keyboard
-import math
-from ewmh import EWMH
+from pouple import Pouple
 
 
-ewmh = EWMH()
-width, height = ewmh.getDesktopGeometry()
+if __name__ == '__main__':
+    pouple = Pouple.create()
 
+    print(pouple)
 
-def frame(client):
-    frame = client
-    while frame.query_tree().parent != ewmh.root:
-        frame = frame.query_tree().parent
-    return frame
+    keyboard.add_hotkey('ctrl+left arrow', pouple.alignLeft)
+    keyboard.add_hotkey('ctrl+right arrow', pouple.alignRight)
+    keyboard.add_hotkey('ctrl+up arrow', pouple.alignTop)
+    keyboard.add_hotkey('ctrl+down arrow', pouple.alignBottom)
 
-def align(x, y, width, height, type):
-    win = frame(ewmh.getActiveWindow())
+    keyboard.add_hotkey('ctrl+alt+f', pouple.center)
 
-    ewmh.setMoveResizeWindow(win, 0, x, y, width, height)
-    ewmh.setWmState(win, 1, type + '+_NET_WM_STATE_REMOVE')
-
-    ewmh.display.flush()
-
-def center():
-    win = ewmh.getActiveWindow()
-    g = frame(win).get_geometry();
-
-    x = math.floor((width - g.width) / 2);
-    y = math.floor((height - g.height) / 2);
-    print(height, g.height)
-
-    print(x, y)
-    ewmh.setMoveResizeWindow(win, 0, x, y, g.width, g.height)
-
-    ewmh.display.flush()
-
-
-keyboard.add_hotkey('ctrl+left arrow', lambda: align(0, 0, int(width / 2), height, '_NET_WM_STATE_MAXIMIZED_VERT'))
-keyboard.add_hotkey('ctrl+right arrow', lambda: align(int(width / 2), 0, int(width / 2), height, '_NET_WM_STATE_MAXIMIZED_VERT'))
-keyboard.add_hotkey('ctrl+up arrow', lambda: align(0, 0, width, int(height / 2), '_NET_WM_STATE_MAXIMIZED_HORZ'))
-keyboard.add_hotkey('ctrl+down arrow', lambda: align(0, int(height / 2), width, int(height / 2), '_NET_WM_STATE_MAXIMIZED_HORZ'))
-
-keyboard.add_hotkey('ctrl+alt+f', center)
-
-while True:
-    time.sleep(0.05)
+    while True:
+        time.sleep(0.1)
 
--- pouple.py
+++ pouple.py
@@ -1,23 +1,93 @@
+import math
+import platform
+from ewmh import EWMH
+
+
 class Pouple:
-    def align(x, y, width, height):
+    @staticmethod
+    def create():
+        system = platform.system()
+
+        if system == 'Linux':
+            return PoulpeXlib()
+        elif system == 'Windows':
+            if (platform.architecture()[0] == '32bit'):
+                return PoulpeWin32()
+            else:
+                return PoulpeWin64()
+        else:
+            raise Error("Invalid platform")
+
+
+    def align(self, x, y, width, height, type=''):
         pass
 
-    def alignLeft(percent=50):
+    def alignLeft(self, percent=50):
         pass
 
-    def alignRight(percent=50):
+    def alignRight(self, percent=50):
         pass
 
-    def alignTop(percent=50):
+    def alignTop(self, percent=50):
         pass
 
-    def alignBottom(percent=50):
+    def alignBottom(self, percent=50):
         pass
+
+    def center(self):
+        pass
+
 
 
 class PoulpeXlib(Pouple):
+    def __init__(self):
+        self.ewmh = EWMH()
+        self.width, self.height = self.ewmh.getDesktopGeometry()
+
+    def frame(self, client):
+        frame = client
+        while frame.query_tree().parent != self.ewmh.root:
+            frame = frame.query_tree().parent
+        return frame
+
+    def align(self, x, y, width, height, type):
+        win = self.frame(self.ewmh.getActiveWindow())
+
+        self.ewmh.setMoveResizeWindow(win, 0, x, y, width, height)
+        self.ewmh.setWmState(win, 1, '_NET_WM_STATE_TOGGLE + ' + type)
+
+        self.ewmh.display.flush()
+
+    def alignLeft(self):
+        self.align(0, 0, int(self.width / 2), self.height, '_NET_WM_STATE_MAXIMIZED_VERT')
+
+    def alignRight(self):
+        self.align(int(self.width / 2), 0, int(self.width / 2), self.height, '_NET_WM_STATE_MAXIMIZED_VERT')
+
+    def alignTop(self):
+        self.align(0, 0, self.width, int(self.height / 2), '_NET_WM_STATE_MAXIMIZED_HORZ')
+
+    def alignBottom(self):
+        self.align(0, int(self.height / 2), self.width, int(self.height / 2), '_NET_WM_STATE_MAXIMIZED_HORZ')
+
+    def center(self):
+        win = self.ewmh.getActiveWindow()
+        g = self.frame(win).get_geometry();
+
+        x = math.floor((self.width - g.width) / 2);
+        y = math.floor((self.height - g.height - 30) / 2);
+
+        x = x if x > 0 else 0
+        y = y if y > 0 else 0
+        
+        self.ewmh.setMoveResizeWindow(win, 0, x, y, g.width, g.height)
+
+        self.ewmh.display.flush()
+
+
+
+class PoulpeWin32(Pouple):
     pass
 
-
-class PuulpeWin32(Pouple):
+class PoulpeWin64(Pouple):
     pass
