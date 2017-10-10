24614b59d8ada04c4f317eaef9f9d1c48db82589
hitman <hitman@quartsoft.com>
2017-10-10 12:07:22 +0300

    add install file

---
+++ install.py
@@ -0,0 +1,31 @@
+import pip
+
+__all__ = [
+    "keyboard",
+    "configparser"
+]
+
+windows = ["pypiwin32"]
+
+linux = ["ewmh"]
+
+darwin = []
+
+
+def install(packages):
+    for package in packages:
+        pip.main(['install', package])
+
+
+if __name__ == '__main__':
+    from sys import platform
+
+    install(__all__)
+    if platform == 'windows':
+        install(windows)
+    elif platform.startswith('linux'):
+        install(linux)
+    elif platform == 'darwin':
+        install(darwin)
+    else:
+        raise Exception("Invalid platform")
